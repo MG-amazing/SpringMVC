08192492212921ff2f61c1420775e18f7af207d6
MG-amazing <99497704+MG-amazing@users.noreply.github.com>
2022-09-26 18:27:09 +0800

    Initial commit

---
+++ springmvc_11_page/src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -0,0 +1,35 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.BookDao;
+import com.itheima.domain.Book;
+import com.itheima.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookDao bookDao;
+
+    public boolean save(Book book) {
+        return bookDao.save(book)>0;
+    }
+
+    public boolean update(Book book) {
+        return bookDao.update(book)>0;
+    }
+
+    public boolean delete(Integer id) {
+        return bookDao.delete(id)>0;
+    }
+
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
